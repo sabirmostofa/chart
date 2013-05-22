0b8d5905e561cf866598e2f43317fb015c738904
cacophonix <cacophonix@linux.(none)>
2013-05-23 00:12:49 +0200

    done by scatter chart working

--- main.php
+++ main.php
@@ -101,20 +101,26 @@ $x = @_first($x);
 $co =0;
  foreach($f_ar as $k => $v){
 	 $MyData ->addPoints($v['y'], $k);
+	 
 	  $MyData->setScatterSerie("X", $k, $co++); 
-	   $MyData->setSerieOnAxis("X", 0);
+	  $MyData->setScatterSerieDescription($co-1,"Percentile $k"); 
+	  $MyData->setScatterSerieWeight($co-1, 2);
+	  $MyData->setSerieOnAxis("X", 0);
+	   
 	   $MyData->setSerieOnAxis($k, 1);
+	   
 	// break;
 	 
  }
  
- //test points
+ //test values
  
  $MyData->addPoints(array(50, 60, 70), 'l');	
  $MyData->addPoints(array(10, 19, 15), 'w');	
  
  $MyData->setScatterSerie('l', 'w', 114); 
  $MyData->setSerieOnAxis("w", 1);
+ $MyData->setScatterSerieWeight(114,3);
 
  
  
@@ -125,31 +131,31 @@ $co =0;
 
  
 
-//$myPicture = new pImage(900,1000,$MyData); 
+//$myPicture = new pImage(400,400,$MyData); 
  /* Create the pChart object */ 
- $myPicture = new pImage(400,400,$MyData); 
+ $myPicture = new pImage(900,900,$MyData); 
 
  /* Draw the background */ 
  $Settings = array("R"=>170, "G"=>183, "B"=>87, "Dash"=>1, "DashR"=>190, "DashG"=>203, "DashB"=>107); 
- $myPicture->drawFilledRectangle(0,0,400,400,$Settings); 
+ $myPicture->drawFilledRectangle(0,0,900,900,$Settings); 
 
  /* Overlay with a gradient */ 
  $Settings = array("StartR"=>219, "StartG"=>231, "StartB"=>139, "EndR"=>1, "EndG"=>138, "EndB"=>68, "Alpha"=>50); 
- $myPicture->drawGradientArea(0,0,400,400,DIRECTION_VERTICAL,$Settings); 
- $myPicture->drawGradientArea(0,0,400,20,DIRECTION_VERTICAL,array("StartR"=>0,"StartG"=>0,"StartB"=>0,"EndR"=>50,"EndG"=>50,"EndB"=>50,"Alpha"=>80));
+ $myPicture->drawGradientArea(0,0,900,900,DIRECTION_VERTICAL,$Settings); 
+ $myPicture->drawGradientArea(0,0,900,20,DIRECTION_VERTICAL,array("StartR"=>0,"StartG"=>0,"StartB"=>0,"EndR"=>50,"EndG"=>50,"EndB"=>50,"Alpha"=>80));
 
  /* Write the picture title */  
- $myPicture->setFontProperties(array("FontName"=>"pchart/fonts/Silkscreen.ttf","FontSize"=>6)); 
+ $myPicture->setFontProperties(array("FontName"=>"pchart/fonts/Silkscreen.ttf","FontSize"=>10)); 
  $myPicture->drawText(10,13,"drawScatterLineChart() - Draw a scatter line chart",array("R"=>255,"G"=>255,"B"=>255)); 
 
  /* Add a border to the picture */ 
- $myPicture->drawRectangle(0,0,399,399,array("R"=>0,"G"=>0,"B"=>0)); 
+ $myPicture->drawRectangle(0,0,899,899,array("R"=>0,"G"=>0,"B"=>0)); 
 
  /* Set the default font */ 
- $myPicture->setFontProperties(array("FontName"=>"pchart/fonts/pf_arma_five.ttf","FontSize"=>6)); 
+ $myPicture->setFontProperties(array("FontName"=>"pchart/fonts/pf_arma_five.ttf","FontSize"=>10)); 
   
  /* Set the graph area */ 
- $myPicture->setGraphArea(50,50,350,350); 
+ $myPicture->setGraphArea(50,50,850,850); 
 
  /* Create the Scatter chart object */ 
  $myScatter = new pScatter($myPicture,$MyData); 
@@ -164,7 +170,7 @@ $co =0;
 $myScatter->drawScatterLineChart();
 
  /* Draw the legend */ 
- $myScatter->drawScatterLegend(280,380,array("Mode"=>LEGEND_HORIZONTAL,"Style"=>LEGEND_NOBORDER)); 
+ $myScatter->drawScatterLegend(80,880,array("Mode"=>LEGEND_HORIZONTAL,"Style"=>LEGEND_NOBORDER)); 
 
  /* Render the picture (choose the best way) */ 
  $myPicture->autoOutput("pictures/example.drawScatterLineChart.png");  
